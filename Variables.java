5850ed7f89055959b91d8d8e0337482eca2a4e9e
Allan Branson <bransonaallan@gmail.com>
2023-12-12 16:56:31 +0300

    primitive data types

--- Variables.java
+++ Variables.java
@@ -15,5 +15,30 @@ public class Variables {
         boolean myBool = false;
         System.out.println(myBool);
 
+        int x = 45;
+        int y = 60;
+        System.out.println( x + y );
+
+        String firstName = "Allan";
+        String secondName = "Branson";
+        System.out.println("My names are: "+ firstName + " " + secondName);
+
+        /*Primitive data types
+         * These are data types which when declared require the type and size specification
+         * They are: int, boolean, char, float,long, double, byte
+         */
+
+         int z = 30;
+         boolean bool2 = false;
+         char letter = 'q';
+         float gravity = 9.8f;
+         double complex = 7.8900036;
+         long negative = -89900;
+
+         System.out.println(z);
+         System.out.println(bool2);
+         System.out.println( letter + gravity + complex + negative );
+
+
     }
 }
