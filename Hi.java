3ce54f16b7e6efbca35a79867358e02277de186c
Allan Branson <bransonaallan@gmail.com>
2023-12-12 15:39:33 +0300

    printVsprintln

---
+++ Hi.java
@@ -0,0 +1,19 @@
+public class Hi {
+    public static void main (String [] args){
+        System.out.println("Hello Java, Here we are again");
+        System.out.print("Okay, this is something new. ");
+        System.out.println("Hello Java, Here we are again");
+        System.out.print("Okay, this is something new ");
+        System.out.println(" ");
+        System.out.println(3 + 3);
+        System.out.println(79);
+        System.out.println(30*50);
+        //This is a single line comment
+        /* These
+         * Are
+         * Multi-line
+         * Comments
+         */
+
+    }
+}
