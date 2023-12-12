b73b581f8af1d448f547605272fbd2e084c0c7a8
Allan Branson <bransonaallan@gmail.com>
2023-12-12 16:06:57 +0300

    Variables

---
+++ Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+    public static void main(String[]Args){
+        int myNum = 14;
+        System.out.println(myNum);
+
+        String myName = "Branson";
+        System.out.println("My name is : " + myName);
+
+        float pie = 3.142f;
+        System.out.println( pie );
+
+        char initials = 'A';
+        System.out.println(initials);
+
+        boolean myBool = false;
+        System.out.println(myBool);
+
+    }
+}
