23d7516586bfce1a443812815ae79d2eddba3983
Allan Branson <bransonaallan@gmail.com>
2024-01-01 16:05:48 +0300

    packages

---
+++ Packages.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Packages {
+    public static void main (String [] Args) {
+        Scanner myObj = new Scanner (System.in);
+        System.out.println("Enter a username: ");
+
+        String userName = myObj.nextLine();
+        System.out.println("Username is: " + userName);
+
+        
+    }
+}
